e0884039a23828aa521e5063d602286167a8bbd9
Jensen Ruud <jensen.is.testing@gmail.com>
2022-06-08 16:32:42 -0500

    Added Hero component parts, overlaps the shadow from the Navbar component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,15 @@
 import React from "react";
+import hero_img from "../images/airbnb_clone_hero.png";
 
 export default function Hero() {
   return (
-    <div>
-      <h1>Hero stuff goes here</h1>
+    <div className="hero">
+      <img src={hero_img} className="hero--img" alt="Cool stuff to do"></img>
+      <h1 className="hero--title">Online Experiences</h1>
+      <h4 className="hero--desc">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </h4>
     </div>
   );
 }
